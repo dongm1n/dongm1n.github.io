e6ba0568a26dbf66f781b2c9a3f1c20117518168
dongm1n <dongminso@hotmail.com>
2017-02-06 06:10:32 -0500

    icansleep

--- RANDOMQUOTE/randomquote.js
+++ RANDOMQUOTE/randomquote.js
@@ -1,21 +1,22 @@
-$(document).ready(function() {
-  $('.box').hide();
-  $('#generator').click(function() {
-    $('.box').hide();
-    $('.quote').text("");
-    $('.author').text("");
-    $.ajax({
-      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
-      type: "GET",
-      dataType: 'json',
-      success: function(data) {
-        $('.quote').text('"' + data.quote + '"');
-        $('.author').text('--' + data.author);
-        $('.box').addClass('animated fadeInLeft').show();
-      },
-      beforeSend: function(xhr) {
-        xhr.setRequestHeader("X-Mashape-Authorization", "k0yyUeZkPpmshDLnY9BD8bFhgm5mp15QiNXjsncA14OFYnCyOH")
-      }
-    })
-  })
+// Random Quote Generator
+
+var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+
+var fetchQuote = function(data){
+    if(data.quoteAuthor == ''){
+    data.quoteAuthor = 'Unknown Author';
+  }
+  $(".Author").text(data.quoteAuthor);
+  $(".Quote").text(data.quoteText);
+
+  var tweet = 'https://twitter.com/intent/tweet?text=' + data.quoteText + ' ' + data.quoteAuthor;
+
+  $(".twitter-share").attr("href", tweet);
+}
+$(document).ready(function(){
+   $.getJSON(url, fetchQuote, 'jsonp');
+});
+
+$("#fetch").click(function(){
+   $.getJSON(url, fetchQuote, 'jsonp')
 });
